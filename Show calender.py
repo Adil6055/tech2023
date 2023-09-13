c4704d7a522dc57bfe0c70ddbc3d696c761f97d1
Adil6055 <76851008+Adil6055@users.noreply.github.com>
2023-09-13 22:19:47 +0300

    Update Show calender.py
    123

--- Show calender.py
+++ Show calender.py
@@ -9,11 +9,9 @@
 # ---------------------------------------
 
 import calendar
-<<<<<<< HEAD
+
 inputyear = int(input("Enter the Year you want to see calender for remove feature 1 : "))
-=======
-inputyear = int(input("Enter the Year you want to see calender for feature 1 : "))
->>>>>>> main
+
 year=inputyear
 
 inputmonth =  int(input("Enter month you want to see calender of : "))
