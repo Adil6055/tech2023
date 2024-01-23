20a7ee9cb761a6ddd276d2fa466028a5eaa73180
adil6055 <adil6055@outlook.com>
2024-01-23 09:58:23 +0300

    test1

--- test1.py
+++ test1.py
@@ -1,8 +1,10 @@
-a = 5
-b = 66.2
-c = 15.66
-d = 13.3
+# a = 5
+# b = 66.2
+# c = 15.66
+# d = 13.3
 
-print (round(a*d+c)) 
+# print (round(a*d+c)) 
 
-print(float(a+b+b))
+# print(float(a+b+b))
+
+from escpos.printer import 
